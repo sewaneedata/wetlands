66906604c20596a8b44c276759d55993c307aacf
Tessa Shackelford <shackth0@sewanee.edu>
2022-07-11 13:37:57 -0500

    dropdown variable definitions

--- app.R
+++ app.R
@@ -218,17 +218,37 @@ ui <- dashboardPage(skin = 'black',
       # First tab content
       tabItem(tabName = "overview",
               fluidRow(
-                box(title = "Overview",
+                box(title = "Wetlands Project Overview",
                     solidHeader = TRUE,
                     width = 12,
                     background = "orange")
               ),
               fluidRow(
-                tabBox(
+                tabBox(title = "Our Project",
                   
                   id = "tabset2",
-                  tabPanel(title = "The project"),
-                  tabPanel(title = "Variables")
+                  width = 4,
+                  tabPanel(title = "Project Summary"),
+                  tabPanel(title = "Water Quality Variables",
+                           fluidRow(column(8, h3("Water Quality Variables and Criteria",
+                                                 br(),
+                                                 br(),
+                                                 br(),
+                                                 box(
+                                                   title = "Select Variable:",
+                                                   background = "light-blue",
+                                                   collapsible = TRUE,
+                                                   width = 10,
+                                                   
+                                                   solidHeader = TRUE,
+                                                   
+                                                   selectInput("variable10", "Variables",
+                                                               choices = c("Cond µS/cm", "ORP mV", "pH", "Turbidity NTU", "NitraLED mg/L", "ODO mg/L",
+                                                                           "Temp °C", "NH4+ -N mg/L", "NH3 mg/L" )))),
+                                                  
+                             
+                           ))),
+                  tabPanel(title = "Wetland Photos")
                 )
               ),
               fluidRow(
@@ -247,7 +267,7 @@ ui <- dashboardPage(skin = 'black',
                                                 br(),
                                                 br(),
                                                 h3("Harrison Lowery C '24"),
-                                                p("Harrison Lowery(C'24) is from Homewood, AL and is an English major at Sewanee: The University of the South. Harrison is a member of the Sewanee-Monteagle Rotary Club and a member of Phi Gamma Delta."),
+                                                p("Harrison Lowery is from Homewood, AL and is an English major at Sewanee: The University of the South. Harrison is a member of the Sewanee-Monteagle Rotary Club and a member of Phi Gamma Delta."),
                                                 br(),
                                                 br(),
                                                 br(),
@@ -255,21 +275,21 @@ ui <- dashboardPage(skin = 'black',
                                                 h3("Tessa Shackelford C '24"),
                                                 p("Tessa Shackelford is from Sewanee, TN and is an Environment & Sustainability major at Sewanee: The University of the South. 
                                                   She is a member of the Order of the Gown, a site leader for the Bonner/Canale program at the university farm, a member of Theta Kappa Phi,
-                                                  and plays for the women's tennis team ."
+                                                  and plays for the women's tennis team."
                                                 ))
                                 )), 
                        tabPanel(title = "Project Mentor",
                                 fluidRow(
                                   column(3,
-                                         fluidRow(tags$img(src = "Catherine.png", width = "100%", alt = "Catherine"))),
-                                  column(5, h3("Dr. Catherine Cavagnaro")),
+                                         fluidRow(tags$img(src = "Catherine.png", width = "90%", alt = "Catherine"))),
+                                  column(5, h3("Dr. Catherine Cavagnaro", align = "left")),
                                   br(),
                                   br(),
                                   br(),
                                   p("Dr. Catherine Cavagnaro is a Professor of Mathematics at Sewanee: 
                                                   The University of the South, and the project mentor for the Sewanee Wetlands Project at DataLab. 
                                                   She holds a B.S from Santa Clara University and a Ph.D. from University of Illinois 
-                                                  at Urbana-Champaign."))),
+                                                  at Urbana-Champaign.", align = "left"))),
                        tabPanel(title = "Community Partners",
                                 fluidRow(
                                   column(3, 
